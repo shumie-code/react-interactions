43f77b99e7ce0782de0882f31ef78ff983b10f45
Petros Shumie <shumie.peter@gmail.com>
2020-02-18 14:39:58 -0500

    Added code to inform end-user to continue scrolling once page is loaded for lazyload. Information is for user to imporove ui/ux experience.

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import './Loading.css';
-
+import React from "react";
+import "./Loading.css";
 
 const Loading = () => (
+  <div>
     <div class="sk-folding-cube">
-    <div class="sk-cube1 sk-cube"></div>
-    <div class="sk-cube2 sk-cube"></div>
-    <div class="sk-cube4 sk-cube"></div>
-    <div class="sk-cube3 sk-cube"></div>
+      <div class="sk-cube1 sk-cube"></div>
+      <div class="sk-cube2 sk-cube"></div>
+      <div class="sk-cube4 sk-cube"></div>
+      <div class="sk-cube3 sk-cube"></div>
+    </div>
+    <h1>Once the squares disappear begin scrolling</h1>
   </div>
 );
 
