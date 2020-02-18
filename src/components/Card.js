8a6b8132bf90b648ce8e9a651d7d88005b6ccaff
Petros Shumie <shumie.peter@gmail.com>
2020-02-18 14:29:36 -0500

    Added lazyload react extension for loading ux data components as we scroll

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,31 +1,34 @@
 import React from "react";
+import LazyLoad from "react-lazyload";
 import "./Card.css";
 
 const Card = props => (
-  <div className={props.card.animation}>
-    <div className="front" onClick={() => props.showBack(props.card)}>
-      <img src="juice.jpg" alt="Avatar" className="card-image" />
-      <div className="container">
+  <LazyLoad height={650} offset={-100}>
+    <div className={props.card.animation}>
+      <div className="front" onClick={() => props.showBack(props.card)}>
+        <img src="juice.jpg" alt="Avatar" className="card-image" />
+        <div className="container">
+          <h3>
+            Vitamin Juice <span className="price">$24.99</span>
+          </h3>
+          <p>
+            Need a jump on your vitamins while drinking? Tired of popping the
+            pills? Drink our vitamin enhanced juice, available in several
+            flavours.
+          </p>
+        </div>
+      </div>
+      <div
+        className="container-back back"
+        onClick={() => props.showFront(props.card)}
+      >
         <h3>
           Vitamin Juice <span className="price">$24.99</span>
         </h3>
-        <p>
-          Need a jump on your vitamins while drinking? Tired of popping the
-          pills? Drink our vitamin enhanced juice, available in several
-          flavours.
-        </p>
+        <p>{props.card.description}</p>
       </div>
     </div>
-    <div
-      className="container-back back"
-      onClick={() => props.showFront(props.card)}
-    >
-      <h3>
-        Vitamin Juice <span className="price">$24.99</span>
-      </h3>
-      <p>{props.card.description}</p>
-    </div>
-  </div>
+  </LazyLoad>
 );
 
 export default Card;
